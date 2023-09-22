9a6340c171bea40d58996bb1f07ed0d45c097db8
ashercodeaccount <asher.work.acc@gmail.com>
2023-09-21 20:38:22 -1000

    Added back motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants.java
@@ -3,10 +3,13 @@ package org.firstinspires.ftc.teamcode;
 public class constants {
 
     public static final class Button {
-        public static final String buttonOne = "buttonOne";
-        public static final String buttonTwo = "buttonTwo";
-        public static final double powerOne = 1;
-        public static final double powerTwo = 1;
+        public static final String frontLeftMotor = "buttonOne";
+        public static final String frontRightMotor = "buttonTwo";
+        public static final String backLeftMotor = "buttonThree";
+        public static final String backRightMotor = "buttonFour";
+
+//        public static final double powerOne = 1;
+//        public static final double powerTwo = 1;
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Button.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Button.java
@@ -12,26 +12,34 @@ public class Button {
      */
     private DcMotor frontLeftMotor;
     private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
 
     public Button() {
     }
 
     public void init(HardwareMap hwMap) {
-        this.frontLeftMotor = hwMap.get(DcMotor.class,constants.Button.buttonOne);
-        this.frontRightMotor = hwMap.get(DcMotor.class,constants.Button.buttonTwo);
+        this.frontLeftMotor = hwMap.get(DcMotor.class,constants.Button.frontLeftMotor);
+        this.frontRightMotor = hwMap.get(DcMotor.class,constants.Button.frontRightMotor);
+        this.backLeftMotor = hwMap.get(DcMotor.class,constants.Button.backLeftMotor);
+        this.backRightMotor = hwMap.get(DcMotor.class,constants.Button.backRightMotor);
         this.frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         this.frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.backLeftMotor .setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.backRightMotor .setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-    public void one() {
-        this.frontLeftMotor.setPower(constants.Button.powerOne);
-    }
-    public void two() {
-        this.frontRightMotor.setPower(constants.Button.powerTwo);
-    }
+//    public void one() {
+//        this.frontLeftMotor.setPower(constants.Button.powerOne);
+//    }
+//    public void two() {
+//        this.frontRightMotor.setPower(constants.Button.powerTwo);
+//    }
     public void stop() {
         this.frontLeftMotor.setPower(0);
         this.frontRightMotor.setPower(0);
+        this.backRightMotor.setPower(0);
+        this.backLeftMotor.setPower(0);
     }
     public void setControl(Gamepad gamepad) {
         double leftPower;
@@ -44,6 +52,8 @@ public class Button {
 
         this.frontLeftMotor.setPower(leftPower);
         this.frontRightMotor.setPower(rightPower);
+        this.backLeftMotor.setPower(leftPower);
+        this.backRightMotor.setPower(rightPower);
 
 //        if (gamepad.a) {
 //            this.one();
